4fd1d4abf8a9321020b5f31e877dc3ce7530661a
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-07-27 11:08:52 -0700

    create component folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Search from './Search/Search.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className='App'>
+      <header>
+        <h1>Google Book Search</h1>
       </header>
-    </div>
+      <Search />
+      <Filters />
+      <BookList />
+    </main>
   );
 }
 
