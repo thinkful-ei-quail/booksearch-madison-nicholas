89acc5a27ad60ea95060152542f93003848b5ee7
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-07-27 12:05:25 -0700

    add static version of app

---
+++ src/BookType/BookType.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class BookType extends React.Component {
+    render() {
+        return (
+                <form className="booktype_form">
+                    <label htmlFor="booktype">Book Type:</label>
+                    <select name="booktype" id="booktype">
+                        <option value="ebooks">ebooks</option>
+                        <option value="free-ebooks">free-ebooks</option>
+                        <option value="full">full</option>
+                        <option value="paid-ebooks">paid-ebooks</option>
+                        <option value="partial">partial</option>
+                    </select>
+                </form>
+        );
+    }
+}
+
+
+export default BookType;
--- src/Filters/Filters.js
+++ src/Filters/Filters.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PrintType from '../PrintType/PrintType.js';
+import BookType from '../BookType/BookType.js';
 
 class Filters extends React.Component {
     render() {
         return (
-            <div>
+            <div className="filterBar">
                 <PrintType />
                 <BookType />
             </div>
